7a2be54d3936178d73a8127e2e5d255afd2c15a9
tim-sendible <tim@sendible.com>
2022-06-08 13:46:49 +0100

    try ganesha nfs

--- setup.sh
+++ setup.sh
@@ -7,3 +7,11 @@ k3d cluster delete workflows-ci
 k3d cluster create --config bootstrap/k3d.conf
 kubectl apply -k bootstrap/argocd
 kubectl -n argocd apply -f bootstrap/app-of-apps
+kubectl -n argocd rollout status statefulset/argocd-application-controller
+kubectl -n argocd rollout status deployment/argocd-repo-server
+
+
+sleep 30
+kubectl -n nfs-server-provisioner rollout status statefulset/nfs-server-provisioner
+kubectl -n argo rollout status deployment/workflow-controller
+kubectl -n argo rollout status deployment/argo-server
