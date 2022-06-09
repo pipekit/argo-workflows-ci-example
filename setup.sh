ab232a0718b68f49b8bb42ea61143e37fd734c7c
tim-sendible <tim@sendible.com>
2022-06-09 09:02:16 +0100

    Put the build template in the correct directory!

--- setup.sh
+++ setup.sh
@@ -12,5 +12,9 @@ kubectl -n argocd apply -f bootstrap/app-of-apps
 
 sleep 30
 kubectl -n nfs-server-provisioner rollout status statefulset/nfs-server-provisioner
+kubectl -n ingress-nginx rollout status deployment/nginx-ingress-nginx-controller
+kubectl -n ingress-nginx rollout status daemonset/svclb-nginx-ingress-nginx-controller
 kubectl -n argo rollout status deployment/workflow-controller
 kubectl -n argo rollout status deployment/argo-server
+
+echo "Complete. You should be able to navigate to https://localhost:8443/workflows/argo?limit=500 in your browser now. (Remember to accept the self-signed SSL cert)."
